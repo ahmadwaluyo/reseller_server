4adfbc798fe02d0b4717dbd18ab5562e39318c94
ahmadwaluyo <ahmad.umb15@gmail.com>
2020-06-18 20:57:12 +0700

    debug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const ControllerUser = require('../controllers/ControllerUser');
 const handleError = require('../middlewares/handlleError');
 const authentication = require('../middlewares/authentication');
 
-console.log('masuk route');
 router.post('/', ControllerUser.login);
 router.use(authentication);
 router.post('/register', ControllerUser.register);
