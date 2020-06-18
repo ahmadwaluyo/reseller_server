844299999c44c85118ce0d93df7756162b451c2e
ahmadwaluyo <ahmad.umb15@gmail.com>
2020-06-18 20:52:26 +0700

    try debug

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const handleError = require('../middlewares/handlleError');
 const authentication = require('../middlewares/authentication');
 
 console.log('masuk route');
-router.use(handleError);
 router.post('/', ControllerUser.login);
 router.use(authentication);
 router.post('/register', ControllerUser.register);
+router.use(handleError);
 
 module.exports = router;
