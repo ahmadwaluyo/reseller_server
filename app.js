d12516bf3f34ff1dff6313f0150f8905d3ed4a2e
ahmadwaluyo <ahmad.umb15@gmail.com>
2020-06-18 20:47:48 +0700

    add env file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const router = require('./routes');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
